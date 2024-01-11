6479d60d9d4588a9120f39c941e5c2a9f77782f5
Saranga Chamod <91281208+SarangaChamod@users.noreply.github.com>
2024-01-11 13:54:35 +0530

    Updated MainScreen UI v.1.3

--- src/MainScreen/MainScreen.js
+++ src/MainScreen/MainScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -8,19 +8,44 @@ import {
   Platform,
   StatusBar,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 
 const MainScreen = () => {
+  const [message, sendMessage] = useState([
+    { role: "system", content: "You are a helpful assistant." },
+    { role: "user", content: "Hello" },
+    { role: "assistant", content: "Hello there, how can I help you" },
+  ]);
   const onSend = () => {
     console.warn("Send");
   };
   return (
     <SafeAreaView style={styles.container}>
+      <FlatList
+        data={message}
+        contentContainerStyle={{ gap: 5, padding: 10 }}
+        renderItem={({ item }) => (
+          <View
+            style={[
+              styles.message,
+              {
+                marginLeft: item.role === "user" ? "auto" : 0,
+                backgroundColor: item.role === "user" ? "#10A37F" : "#536967",
+              },
+            ]}
+          >
+            <Text style={styles.messageText}>{item.content}</Text>
+          </View>
+        )}
+      />
+
       <View style={styles.footer}>
         <TextInput placeholder="How can I help you" style={styles.textinput} />
+
         <View style={{ marginRight: 8 }} />
         <TouchableOpacity onPress={onSend} style={styles.send}>
-          <Text>Send</Text>
+          <Text style={styles.send_text}>Send</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -31,12 +56,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "#283033", 
   },
 
   textinput: {
     borderRadius: 50,
-    borderWidth: 1,
-    borderColor: "#10A37F",
+    backgroundColor: '#536967',
     padding: 8,
     flex: 1,
   },
@@ -46,10 +71,23 @@ const styles = StyleSheet.create({
     padding: 13,
     // margin: 2,
   },
+  send_text: {
+    color: "white",
+  },
+  message: {
+    backgroundColor: "gainsboro",
+    padding: 13,
+    borderRadius: 25,
+    width: "80%",
+  },
+  messageText: {
+    color: "white",
+  },
   footer: {
     marginTop: "auto",
     flexDirection: "row",
     padding: 10,
+    backgroundColor: '#38454B'
   },
 });
 
