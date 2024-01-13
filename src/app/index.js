984f4fdaf8ec15ccec621252fb3c327a31d3fda6
Saranga Chamod <91281208+SarangaChamod@users.noreply.github.com>
2024-01-13 12:20:28 +0530

    Expo Routing v.1.1

--- src/app/index.js
+++ src/app/index.js
@@ -1,97 +1,32 @@
-import React, { useState } from "react";
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Platform,
-  StatusBar,
-  TouchableOpacity,
-  FlatList,
-} from "react-native";
-import Message from "../components/Message";
+// App.js
 
-const MainScreen = () => {
-  const [message, sendMessage] = useState([
-    { role: "system", content: "You are a helpful assistant." },
-    { role: "user", content: "Hello" },
-    { role: "assistant", content: "Hello there, how can I help you" },
-  ]);
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import MainScreen from '../../src/MainScreen/MainScreen'; // Import your main screen component
+import SplashScreen from '../../src/SplashScreen/SplashScreen';
 
-  const [prompt, setPrompt] = useState("");
-  const onSend = () => {
-    // console.warn('Send :', prompt);
 
-    sendMessage((existingMessage) => [
-      ...existingMessage,
-      { role: "user", content: prompt },
-    ]);
+export default function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    setPrompt(''); 
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true); // Set isLoaded to true after the timeout
+    }, 3000); 
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={message}
-        contentContainerStyle={{ gap: 5, padding: 10 }}
-        renderItem={({ item }) => <Message message={item} />}
-      />
-
-      <View style={styles.footer}>
-        <TextInput
-          value={prompt}
-          onChangeText={setPrompt}
-          placeholder="How can I help you ?"
-          style={styles.textinput}
-        />
-
-        <View style={{ marginRight: 5 }} />
-        <TouchableOpacity onPress={onSend} style={styles.send}>
-          <Text style={styles.send_text}>Send</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      {isLoaded ? <MainScreen /> : <SplashScreen />}
+      {/* <SplashScreen/> */}
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    backgroundColor: "#283033",
-  },
-
-  textinput: {
-    borderRadius: 50,
-    backgroundColor: "#536967",
-    padding: 8,
-    flex: 1,
-  },
-  send: {
-    borderRadius: 100,
-    backgroundColor: "#10A37F",
-    padding: 13,
-    // margin: 2,
-  },
-  send_text: {
-    color: "white",
-  },
-  message: {
-    backgroundColor: "gainsboro",
-    padding: 13,
-    borderRadius: 25,
-    width: "80%",
-  },
-  messageText: {
-    color: "white",
-  },
-  footer: {
-    marginTop: "auto",
-    flexDirection: "row",
-    padding: 10,
-    backgroundColor: "#38454B",
   },
 });
-
-export default MainScreen;
